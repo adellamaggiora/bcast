49a3f34e488e9b79b74ab11e918e226c2c78e0fb
adellamaggiora <a.dellamaggiora@caen.it>
2023-03-15 15:32:43 +0100

    added app version

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,14 +1,16 @@
 import { Component } from '@angular/core';
+import packageJson from '../../package.json';
+
 @Component({
   selector: 'app-root',
   templateUrl: 'app.component.html'
 })
 export class AppComponent {
 
+  public appVersion: string = packageJson.version;
   public appPages = [
     { title: 'Profile', url: '/profile', icon: 'settings' },
     { title: 'Broadcasts', url: '/bcast', icon: 'earth' }
   ];
-
 
 }
--- src/app/modules/features/bcast/bcast-routing.module.ts
+++ src/app/modules/features/bcast/bcast-routing.module.ts
@@ -1,9 +1,24 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { BcastComponent } from './components/bcast/bcast.component';
+import { CandidateComponent } from './components/candidate/candidate.component';
+import { JoinedComponent } from './components/joined/joined.component';
 
 const routes: Routes = [
-  { path: '', component: BcastComponent, pathMatch: 'full' }
+  {
+    path: '',
+    component: BcastComponent,
+    children: [
+      {
+        path: 'candidate',
+        component: CandidateComponent
+      },
+      {
+        path: 'joined',
+        component: JoinedComponent
+      }
+    ]
+  }
 ];
 
 @NgModule({
