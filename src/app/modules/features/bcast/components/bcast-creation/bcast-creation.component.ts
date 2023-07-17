2191cced0c1537a0dcef5da4b4a9633ffa1703e9
adellamaggiora <a.dellamaggiora@caen.it>
2023-07-17 17:31:47 +0200

    icons changed

---
+++ src/app/modules/features/bcast/components/bcast-creation/bcast-creation.component.ts
@@ -0,0 +1,14 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-bcast-creation',
+  templateUrl: './bcast-creation.component.html',
+  styleUrls: ['./bcast-creation.component.scss'],
+})
+export class BcastCreationComponent  implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {}
+
+}
