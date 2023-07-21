b3325a5ca10dda88ea43c961f117e84393fce007
adellamaggiora <a.dellamaggiora@caen.it>
2023-07-21 18:25:19 +0200

    added ngOnChanges tip

--- src/app/modules/shared/ui-kit/components/bcast-card/bcast-card.component.ts
+++ src/app/modules/shared/ui-kit/components/bcast-card/bcast-card.component.ts
@@ -1,4 +1,4 @@
-import { Component, EventEmitter, Input, Output } from '@angular/core';
+import { Component, EventEmitter, Input, OnChanges, Output, SimpleChange } from '@angular/core';
 import dateFns from 'src/functions/date-fns';
 import { IListedBcast } from 'src/interfaces/listed-bcast';
 
@@ -7,7 +7,7 @@ import { IListedBcast } from 'src/interfaces/listed-bcast';
   templateUrl: './bcast-card.component.html',
   styleUrls: ['./bcast-card.component.scss'],
 })
-export class BcastCardComponent {
+export class BcastCardComponent implements OnChanges {
 
   @Input() listedBcast: IListedBcast;
   @Input() userTag: string[];
@@ -18,6 +18,12 @@ export class BcastCardComponent {
   public dateFns = dateFns;
 
   constructor() { }
+
+  ngOnChanges(change) {
+    // in questo hook puoi intercettare tutti i cambiamenti sulle prop di input @Input()
+    console.log('ngOnChanges hook');
+    console.log(change)
+  }
 
   onJoin() {
     this.join.emit(this.listedBcast.id);
