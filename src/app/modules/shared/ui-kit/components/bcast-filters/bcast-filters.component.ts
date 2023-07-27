d39d4cde1ed31859d3ab4b77e5f4e59f795747ec
adellamaggiora <a.dellamaggiora@caen.it>
2023-07-27 16:50:20 +0200

    filter modal code scaffolding

--- src/app/modules/shared/ui-kit/components/bcast-filters/bcast-filters.component.ts
+++ src/app/modules/shared/ui-kit/components/bcast-filters/bcast-filters.component.ts
@@ -1,14 +1,32 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, ViewChild } from '@angular/core';
+import { IonModal } from '@ionic/angular';
+import { OverlayEventDetail } from '@ionic/core/components';
 
 @Component({
   selector: 'app-bcast-filters',
   templateUrl: './bcast-filters.component.html',
   styleUrls: ['./bcast-filters.component.scss'],
 })
-export class BcastFiltersComponent  implements OnInit {
+export class BcastFiltersComponent {
+
+  @ViewChild(IonModal) modal: IonModal;
+  name: string;
 
   constructor() { }
 
-  ngOnInit() {}
+  cancel() {
+    this.modal.dismiss(null, 'cancel');
+  }
+
+  confirm() {
+    this.modal.dismiss(this.name, 'confirm');
+  }
+
+  onWillDismiss(event: Event) {
+    const ev = event as CustomEvent<OverlayEventDetail<string>>;
+    if (ev.detail.role === 'confirm') {
+      window.alert(`Hello, ${ev.detail.data}!`);
+    }
+  }
 
 }
