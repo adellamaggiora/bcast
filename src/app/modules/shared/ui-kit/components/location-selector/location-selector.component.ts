f6c56c421c458fbb4169322bb01175a40af1800b
adellamaggiora <a.dellamaggiora@caen.it>
2023-07-28 14:29:32 +0200

    location selector code scaffolding

---
+++ src/app/modules/shared/ui-kit/components/location-selector/location-selector.component.ts
@@ -0,0 +1,37 @@
+import { Component, EventEmitter, Output, ViewChild } from '@angular/core';
+import { IonModal } from '@ionic/angular';
+import { IGeoLocation } from 'src/interfaces/geo-location';
+
+@Component({
+  selector: 'app-location-selector',
+  templateUrl: './location-selector.component.html',
+  styleUrls: ['./location-selector.component.scss'],
+})
+export class LocationSelectorComponent {
+
+  @ViewChild(IonModal) modal: IonModal;
+
+  location: IGeoLocation;
+  @Output() selectedLocation = new EventEmitter<IGeoLocation>(null);
+
+  constructor() { }
+
+  onSelectedLocation(location: IGeoLocation) {
+    this.location = location;
+    this.modal.dismiss();
+  }
+
+  clearLocation() {
+    this.location = null;
+    this.emitLocation();
+  }
+
+  emitLocation() {
+    this.selectedLocation.emit(this.location);
+  }
+
+  openModal() {
+    this.modal.present()
+  }
+
+}
